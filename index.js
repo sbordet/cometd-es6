f0fc292111098855a19ffe51e7b9e1dd728a84e7
Sean Kelley <skelley@palantir.com>
2016-02-16 14:41:07 -0800

    Port 'common' files to CommonJS as of 06a8d3d83224b8dd82fe07213402201186ae02ff.

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = require('./CometD');
+module.exports.Transport = require('./Transport');
+module.exports.RequestTransport = require('./RequestTransport');
+module.exports.TransportRegistry = require('./TransportRegistry');
+module.exports.CallbackPollingTransport = require('./CallbackPollingTransport');
+module.exports.LongPollingTransport = require('./LongPollingTransport');
+module.exports.WebSocketTransport = require('./WebSocketTransport');
